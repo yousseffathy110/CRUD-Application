d759df7bb8b5dd8ec8dd9f8c3abe1f8bbb8c6c62
yousef fathy <yf1102003@gmail.com>
2024-08-07 21:04:33 +0300

    feat: finsh the add product functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 const title = document.getElementById("title"),
   price = document.getElementById("price"),
   taxes = document.getElementById("taxes"),
@@ -9,12 +9,46 @@ const title = document.getElementById("title"),
   category = document.getElementById("category"),
   submit = document.getElementById("submit");
 
+//calc the total price
 const getTotalPrice = () => {
-  console.log("clicked");
-  if (price.value === "") alert("Please select a price");
-  else {
+  if (price.value !== "") {
     let totalPrice =
       (+price.value + +ads.value + +taxes.value) * (1 - +discount.value / 100);
-    total.textContent = Math.trunc(totalPrice);
-  }
+    total.textContent = `${Math.trunc(totalPrice)} LE`;
+  } else total.innerHTML = "Total:";
 };
+
+// clear inputs
+const clearInputs = () => {
+  document.querySelectorAll("input").forEach((element) => {
+    element.value = "";
+  });
+};
+
+let dataProducts;
+
+if (localStorage.product != null)
+  dataProducts = JSON.parse(localStorage.product);
+else dataProducts = [];
+
+// add new product
+const addProduct = () => {
+  const newProduct = {
+    title: title.value,
+    price: price.value,
+    taxes: taxes.value,
+    ads: ads.value,
+    discount: discount.value,
+    total: total.textContent,
+    count: count.value,
+    category: category.value,
+  };
+
+  dataProducts.push(newProduct);
+  localStorage.setItem("product", JSON.stringify(dataProducts));
+
+  // clear inputs
+  clearInputs();
+};
+
+submit.addEventListener("click", addProduct);
